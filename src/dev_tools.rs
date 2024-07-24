83f4963c38e266f2875aa4491c4f772dfab7ac42
Jiří H. <jiri@bacehub.de>
2024-07-24 21:00:40 +0200

    add bevy_inspector_egui

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,12 +1,13 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
 use bevy::{dev_tools::states::log_transitions, prelude::*};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use crate::screen::Screen;
 
 pub(super) fn plugin(app: &mut App) {
-    // Print state transitions in dev builds
-    app.add_systems(Update, log_transitions::<Screen>)
+    app.add_plugins(WorldInspectorPlugin::new())
+        .add_systems(Update, log_transitions::<Screen>)
         .add_systems(Update, process_score_raise_input)
         .add_plugins(avian2d::debug_render::PhysicsDebugPlugin::default());
 }
