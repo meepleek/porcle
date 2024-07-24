a187ab80b2356bf8b8963df32ddfcf5555133824
Jiří H. <jiri@bacehub.de>
2024-07-24 21:07:53 +0200

    dev_tools - allow toggling editor

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,12 +1,14 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
-use bevy::{dev_tools::states::log_transitions, prelude::*};
+use bevy::{
+    dev_tools::states::log_transitions, input::common_conditions::input_toggle_active, prelude::*,
+};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 use crate::screen::Screen;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins(WorldInspectorPlugin::new())
+    app.add_plugins(WorldInspectorPlugin::new().run_if(input_toggle_active(false, KeyCode::KeyE)))
         .add_systems(Update, log_transitions::<Screen>)
         .add_systems(Update, process_score_raise_input)
         .add_plugins(avian2d::debug_render::PhysicsDebugPlugin::default());
