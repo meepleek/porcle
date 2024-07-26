35cc62517f6619b77beb1d9a703082018409349a
Jiří H. <jiri@bacehub.de>
2024-07-26 23:32:46 +0200

    devtool ammo binding

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -7,25 +7,21 @@ use bevy::{
 #[cfg(feature = "dev")]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
-use crate::screen::Screen;
+use crate::{game::spawn::paddle::PaddleAmmo, screen::Screen};
 
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(Update, log_transitions::<Screen>)
-        .add_systems(Update, process_score_raise_input)
+        .add_systems(Update, process_debug_input)
         .add_plugins(avian2d::debug_render::PhysicsDebugPlugin::default());
 
     #[cfg(feature = "dev")]
     app.add_plugins(WorldInspectorPlugin::new().run_if(input_toggle_active(false, KeyCode::KeyE)));
 }
 
-fn process_score_raise_input(_input: Res<ButtonInput<KeyCode>>) {
-    // if let Some(_score) = if input.just_pressed(KeyCode::NumpadAdd) {
-    //     Some(())
-    // } else if input.just_pressed(KeyCode::NumpadSubtract) {
-    //     Some(())
-    // } else {
-    //     None
-    // } {
-    //     // todo
-    // }
+fn process_debug_input(input: Res<ButtonInput<KeyCode>>, mut ammo_q: Query<&mut PaddleAmmo>) {
+    if input.just_pressed(KeyCode::NumpadAdd) {
+        for mut ammo in &mut ammo_q {
+            ammo.0 += 10;
+        }
+    }
 }
